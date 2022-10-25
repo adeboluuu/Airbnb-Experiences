799d9feccc7d827b13a0321a592d8902bcfad8a7
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-10-25 14:41:57 +0100

    added the flex to the cards

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const cards = data.map(item => {
     return (
         <Card 
+            key={item.id}
             img={item.coverImg}
             rating={item.stats.rating}
             reviewCount={item.stats.reviewCount}
@@ -22,8 +23,9 @@ function App() {
     <div className="App">
       <Nav/>
       <Hero/>
-      {cards}
-  
+      <section className="cards-list">
+                {cards}
+            </section>
     </div>
   )
 }
--- src/data.js
+++ src/data.js
@@ -17,9 +17,9 @@ export default [
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        coverImg: "wedding-photography 1.png",
+        coverImg: "wedding-photography.png",
         stats: {
-            rating: 5.0,
+            rating: 4.0,
             reviewCount: 30
         },
         location: "Online",
@@ -30,7 +30,7 @@ export default [
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "mountain-bike 1.png",
+        coverImg: "mountain-bike.png",
         stats: {
             rating: 4.8,
             reviewCount: 2
