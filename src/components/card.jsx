6a72ca9aa6de398e2d92c18a51a6864e6065ed5b
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-10-26 01:28:58 +0100

    Included the badges

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,15 +3,15 @@ import Star from '../assets/Star 1.png'
 
 const card = (props) => {
   let badgeText
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
       badgeText = "SOLD OUT"
-  }else if (props.location ==="Online"){
+  }else if (props.item.location ==="Online"){
     badgeText = "ONLINE"
 
   }
   return (
     <div className='card'>
-      {badgeText  && <div className="card-badge">{badgeText}</div>}
+      {badgeText && <div className="card-badge">{badgeText}</div>}
         <img src={`/src/assets/${props.item.coverImg}`}   className='card-img'/>
         <div className='card-stats'>
             <img src={Star} alt='Star'className='card-star'/>
